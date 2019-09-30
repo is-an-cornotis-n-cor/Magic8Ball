384d03ec638604cdcd85023bf6d0f4b50ff68598
is-an-cornotis-n-cor <isancor.yadessa.2023@student.lhschools.org>
2019-09-30 09:54:56 -0400

    Added 5 positive and 5 negative statements for each else if and if to main method

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -16,36 +16,36 @@ import java.util.Scanner;
             System.out.println("Now ask me a question");
             question = keyboard.nextLine();
 
-            if (10 >= firstNumber && firstNumber > 0) {
+            if (10 >=firstNumber && firstNumber>0) {
                 System.out.println("No");
-            } else if (20 >= firstNumber && firstNumber > 10) {
+            } else if (20>= firstNumber && firstNumber > 10) {
                 System.out.println("That is pretty good");
 
-            } else if (30 >= firstNumber && firstNumber > 20) {
-                System.out.println("That is pretty good");
+            } else if (30>= firstNumber && firstNumber > 20) {
+                System.out.println("That is not right");
 
-            } else if (40 >= firstNumber && firstNumber > 30) {
-                System.out.println("That is pretty good");
+            } else if (40>= firstNumber && firstNumber > 30) {
+                System.out.println("I would never");
 
-            } else if (50 >= firstNumber && firstNumber > 40) {
-                System.out.println("That is pretty good");
+            } else if (50>= firstNumber && firstNumber > 40) {
+                System.out.println("Yes");
 
-            } else if (60 >= firstNumber && firstNumber > 50) {
-                System.out.println("That is pretty good");
+            } else if (60>= firstNumber && firstNumber > 50) {
+                System.out.println("That is pretty bad");
 
-            } else if (70 >= firstNumber && firstNumber > 60) {
-                System.out.println("That is pretty good");
+            } else if (70>= firstNumber && firstNumber > 60) {
+                System.out.println("That is Awesome");
 
-            } else if (80 >= firstNumber && firstNumber > 70) {
-                System.out.println("That is pretty good");
+            } else if (80>= firstNumber && firstNumber > 70) {
+                System.out.println("Epic");
             }
-            else if (90>= firstNumber && firstNumber > 80) {
-                System.out.println("That is pretty good");
+            else if (90>= firstNumber && firstNumber >80) {
+                System.out.println("That is great");
             }
             else if (100>= firstNumber && firstNumber >90) {
-                System.out.println("That is pretty good");
+                System.out.println("Why would you do that?!");
             }
             else
-                System.out.println("No silly thats not a number between 1 and 100 hehehehehehehehehehe");
+                System.out.println("No silly that's not a number between 1 and 100 hehehehehehehehehehe");
             }
         }
